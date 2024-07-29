97d7c425165b18c8d95ab2eb54be287a3ef7a64d
bind-ritu1211 <kumariritubind@gmail.com>
2024-07-29 17:11:07 +0530

    add todo list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
+import './assets/style/index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App />);
-reportWebVitals();
-
+const DATA = [
+    { id: "todo-0", name: "Banana", completed: true },
+    { id: "todo-1", name: "Apple", completed: false },
+    { id: "todo-2", name: "Orange", completed: false },
+  ];
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App tasks={DATA} />
+    </React.StrictMode>,
+  )
+  
