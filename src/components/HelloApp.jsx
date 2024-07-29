89732e90ad82406962d692d266149bac70157270
bind-ritu1211 <kumariritubind@gmail.com>
2024-07-29 16:34:50 +0530

    configure MUI style

--- src/components/HelloApp.jsx
+++ src/components/HelloApp.jsx
@@ -1,15 +1,20 @@
-import logo from '../assets/images/logo.svg'
-import addTodos from '../utils/common.js'
-
+import logo from "../assets/images/logo.svg";
+import addTodos from "../utils/common.js";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function HelloApp() {
-    return (
-        <div className="HelloApp">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>Todo app</p>
-        </div>
-
-    );
+  return (
+    <div className="HelloApp">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Todo app</p>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  );
 }
 
 export default HelloApp;
